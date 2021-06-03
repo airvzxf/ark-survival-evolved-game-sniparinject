fed0e675b11073e23a5765e0ed79321048c18351
Israel Roldan <israel.alberto.rv@gmail.com>
2021-06-03 00:26:20 -0500

    Added the settings parser package; Added requirements to this Python project.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@
 ARK Game: Read the network packages is an effort to use WireShark and scan the package to know
 your current position and get more information in the play game.
 """
+from core.settings import Settings
 
 if __name__ == '__main__':
-    print("Hello World!")
+    settings = Settings('settings.yml').get_dictionary()
+    print()
+    print('=== Settings ===')
+    print(settings)
