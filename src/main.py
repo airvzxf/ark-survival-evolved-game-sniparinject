4f82a3bdb76880032011a371fcf4f881b7a44488
Israel Alberto RV <israel.alberto.rv@gmail.com>
2021-06-16 00:58:54 -0500

    Added the SniPar Inject package and remove all the unnecessary code.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 ARK Game: Read the network packets is an effort to use WireShark and scan the packet to know
 your current position and get more information in the play game.
 """
-from core.network_sniffer import NetworkSniffer
+from sniparinject.network_sniffer import NetworkSniffer
 
 if __name__ == '__main__':
     SETTINGS_PATH = 'settings.yml'
--- src/script/flake8.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-CURRENT_PATH=$(dirname "${0}")
-cd "${CURRENT_PATH}" || exit
-
-cd ../
-
-./venv/bin/flake8 .
